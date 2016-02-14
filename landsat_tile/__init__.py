02b61e9d843b3249e81b193b8f4ff72e3d526aa0
Chris Holden <ceholden@gmail.com>
2016-02-14 13:00:08 -0500

    Setup logger

--- landsat_tile/__init__.py
+++ landsat_tile/__init__.py
@@ -1,1 +1,13 @@
-from .version import __version__
+import logging
+
+from .version import __version__  # noqa
+
+
+_FORMAT = '%(asctime)s:%(levelname)s:%(lineno)s:%(module)s.%(funcName)s:%(message)s'  # noqa
+_formatter = logging.Formatter(_FORMAT, '%H:%M:%S')
+_handler = logging.StreamHandler()
+_handler.setFormatter(_formatter)
+
+logger = logging.getLogger('landsat_tile')
+logger.addHandler(_handler)
+logger.setLevel(logging.INFO)
