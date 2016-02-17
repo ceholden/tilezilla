47b0345eb6606991d8f703f174790aebcfe901f9
Chris Holden <ceholden@gmail.com>
2016-02-17 15:30:24 -0500

    Add description and repr to TileSpec

--- tilezilla/tilespec.py
+++ tilezilla/tilespec.py
@@ -19,8 +19,10 @@ class TileSpec(object):
         crs (str): coordinate system reference string
         res (tuple): pixel X/Y resolution
         size (tuple): number of pixels in X/Y dimension of each tile
+        desc (str): description of tile specification (default: None)
     """
-    def __init__(self, ul, crs, res, size):
+
+    def __init__(self, ul, crs, res, size, desc=None):
         self.ul = ul
         self.crs = crs
         self.res = res
@@ -28,7 +30,19 @@ class TileSpec(object):
         if not self.crs:
             raise ValueError('Could not parse coordinate reference system '
                              'string to a projection ({})'.format(crs))
+        self.desc = desc or 'unnamed'
         self._tiles = {}
+
+    def __repr__(self):
+        return ("<{name}(desc={desc}, ul={ul}, crs={crs}, "
+                "res={res}, size={size}) at {hex}>".format(
+                    name=self.__class__.__name__,
+                    hex=hex(id(self)),
+                    desc=self.desc,
+                    ul=self.ul,
+                    crs=self.crs,
+                    res=self.res,
+                    size=self.size))
 
     def __getitem__(self, index):
         """ Return a Tile for the grid row/column specified by index
@@ -36,7 +50,6 @@ class TileSpec(object):
         if isinstance(index, tuple):
             if len(index) > 2:
                 raise IndexError('TileSpec only has two dimensions (row/col)')
-            # _row, _col = [], []
             if not isinstance(index[0], int) and isinstance(index[1], int):
                 raise NotImplementedError(
                     'Only support indexing int/int for now')
@@ -55,8 +68,7 @@ class TileSpec(object):
             left=self.ul[0] + index[1] * self.size[0] * self.res[0],
             right=self.ul[0] + (index[1] + 1) * self.size[0] * self.res[0],
             top=self.ul[1] - index[0] * self.size[1] * self.res[1],
-            bottom=self.ul[1] - (index[0] + 1) * self.size[1] * self.res[1]
-        )
+            bottom=self.ul[1] - (index[0] + 1) * self.size[1] * self.res[1])
 
     def _index_to_tile(self, index):
         """ Return the Tile for given index
@@ -93,12 +105,12 @@ class TileSpec(object):
                 yield tile
 
     def _frame_bounds(self, bounds):
-        min_grid_x = (self.ul[0] - bounds.left) // self.size[0]
-        max_grid_x = (self.ul[0] - bounds.right) // self.size[0]
-        min_grid_y = (self.ul[1] - bounds.top) // self.size[1]
-        max_grid_y = (self.ul[1] - bounds.bottom) // self.size[1]
-        return (range(int(min_grid_y), int(max_grid_y) + 1),
-                range(int(min_grid_x), int(max_grid_x) + 1))
+        min_grid_x = int((self.ul[0] - bounds.left) // self.size[0])
+        max_grid_x = int((self.ul[0] - bounds.right) // self.size[0])
+        min_grid_y = int((self.ul[1] - bounds.top) // self.size[1])
+        max_grid_y = int((self.ul[1] - bounds.bottom) // self.size[1])
+        return (range(min_grid_y, max_grid_y + 1),
+                range(min_grid_x, max_grid_x + 1))
 
 
 class Tile(object):
@@ -109,6 +121,7 @@ class Tile(object):
         crs (str): the coordinate reference system of the tile
 
     """
+
     def __init__(self, bounds, crs):
         self.bounds = bounds
         self.crs = crs
@@ -147,7 +160,7 @@ def retrieve_tilespecs():
                                             'data/tile_specs.json').decode())
     for key in tilespecs:
         tilespecs[key]['crs'] = rasterio.crs.from_string(tilespecs[key]['crs'])
-        tilespecs[key] = TileSpec(**tilespecs[key])
+        tilespecs[key] = TileSpec(desc=key, **tilespecs[key])
     return tilespecs
 
 #: dict: Built-in tile specifications available by default
