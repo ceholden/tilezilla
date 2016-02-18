4008d22a54c4a3f7817bf8165ac259aeaa2f7200
Chris Holden <ceholden@gmail.com>
2016-02-18 16:43:55 -0500

    Test _util

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,19 +4,19 @@ import tarfile
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def ESPA_archive(request):
     """ ESPA download archive filename
     """
-    return os.path.join(os.path.basename(__file__), 'data',
+    return os.path.join(os.path.dirname(__file__), 'data',
                         'LT50120312002300-SC20151009172149.tar.gz')
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def ESPA_archive_EPSG5070(request):
     """ ESPA download archive filename, reprojected to EPSG:5070
     """
-    return os.path.join(os.path.basename(__file__), 'data',
+    return os.path.join(os.path.dirname(__file__), 'data',
                         'LT50120312002300-SC20151009172149_EPSG5070.tar.gz')
 
 
@@ -24,15 +24,17 @@ def ESPA_archive_EPSG5070(request):
 def ESPA_order(request, ESPA_archive, tmpdir_factory):
     """ Extract ``ESPA_archive`` to temporary directory
     """
-    path = tmpdir_factory.mktmp('ESPA_order')
+    path = str(tmpdir_factory.mktemp('ESPA_order'))
     with tarfile.open(ESPA_archive) as tgz:
         tgz.extractall(path)
+    return path
 
 
 @pytest.fixture(scope='module')
 def ESPA_order_EPSG5070(request, ESPA_archive_EPSG5070, tmpdir_factory):
     """ Extract ``ESPA_archive_EPSG5070`` to temporary directory
     """
-    path = tmpdir_factory.mktmp('ESPA_order_EPSG5070')
+    path = str(tmpdir_factory.mktemp('ESPA_order_EPSG5070'))
     with tarfile.open(ESPA_archive_EPSG5070) as tgz:
         tgz.extractall(path)
+    return path
