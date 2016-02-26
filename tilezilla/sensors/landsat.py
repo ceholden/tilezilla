a331d20c7d064f2a3970c2b84712bb09970fc3d7
Chris Holden <ceholden@gmail.com>
2016-02-26 10:51:34 -0500

    Simplify MTL file init to classmethods

--- tilezilla/sensors/landsat.py
+++ tilezilla/sensors/landsat.py
@@ -5,21 +5,20 @@ from collections import OrderedDict
 from .._util import lazy_property
 
 
-def parse_MTL(path):
+def parse_MTL(fid):
     """ Return an ordered dict from a Landsat "MTL" metadata file
 
     Args:
-        path (str): MTL filename
+        fid (file descriptor): file descriptor for "MTL" metadata file
 
     Returns:
         OrderedDict: dict of MTL file
     """
     data = OrderedDict()
-    with open(path, 'r') as fid:
-        for line in fid:
-            split = line.split(' = ')
-            if len(split) == 2:
-                data[split[0].strip().strip('"')] = split[1].strip().strip('"')
+    for line in fid:
+        split = line.decode().split(' = ')
+        if len(split) == 2:
+            data[split[0].strip().strip('"')] = split[1].strip().strip('"')
     return data
 
 
@@ -27,12 +26,10 @@ class MTL(object):
     """ Landsat "MTL" metadata file
 
     Args:
-        path (str): path to the MTL file
+        data (OrderedDict): metadata contained within the MTL file
     """
-
-    def __init__(self, path):
-        #: OrderedDict: metadata contained within the MTL file
-        self.data = parse_MTL(path)
+    def __init__(self, data):
+        self.data = data
 
     @lazy_property
     def scene_id(self):
@@ -71,3 +68,26 @@ class MTL(object):
         """ ACCA cloud cover score
         """
         return float(self.data['CLOUD_COVER'])
+
+    @classmethod
+    def from_file(cls, path):
+        """
+        Args:
+            path (str): path to the MTL file
+
+        Returns:
+            MTL: Instance of MTL
+        """
+        with open(path, 'r') as fid:
+            return cls(parse_MTL(fid))
+
+    @classmethod
+    def from_fid(cls, fid):
+        """
+        Args:
+            fid (file descriptor): file descriptor for "MTL" metadata file
+
+        Returns:
+            MTL: Instance of MTL
+        """
+        return cls(parse_MTL(fid))
