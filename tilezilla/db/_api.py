5b5f380853f2f7d9d816930d5663b027be873661
Chris Holden <ceholden@gmail.com>
2016-03-07 14:55:53 -0500

    Store bands in DB

--- tilezilla/db/_api.py
+++ tilezilla/db/_api.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     from tilezilla import tilespec, products, stores, _util
 
     # Create a product
-    prod = products.ESPALandsat('tests/data/LT50120312002300-SC20151009172149_EPSG5070/')
+    prod = products.ESPALandsat.from_path('tests/data/LT50120312002300-SC20151009172149_EPSG5070/')
 
     products = [prod]
 
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     # Create dataset resource
     dataset = DatasetResource(db, datacube)
     # Add / get collection
-    collection_name = prod.__class__.__name__
+    collection_name = prod.description
     datacube.ensure_collection(collection_name)
 
     # Create/get collection for product
@@ -36,53 +36,8 @@ if __name__ == '__main__':
 
     for product in products:
         product_ids = dataset.ensure_product(product)
+        for product_id in product_ids:
+            for band in product.bands:
+                dataset.ensure_band(product_id, band)
 
     from IPython.core.debugger import Pdb; Pdb().set_trace()
-#    # Find and add tiles for product
-#    tiles = list(weld_conus.bounds_to_tile(prod.bounding_box(weld_conus.crs)))
-#    for tile in tiles:
-#        # TODO: wrap up somewhere --> TileResource
-#        sql_tile = Tile(ref_collection_id=sql_collection.id,
-#                        horizontal=tile.horizontal,
-#                        vertical=tile.vertical,
-#                        hv='h{}v{}'.format(tile.horizontal, tile.vertical))
-#        session.add(sql_tile)
-#        session.commit()
-#
-#        # TODO: wrap up somewhere --> ProductResource
-#        product_attrs = dict(
-#            ref_collection_id=sql_collection.id,
-#            ref_tile_id=sql_tile.id,
-#            name=prod.timeseries_id,
-#            platform=prod.platform,
-#            instrument=prod.instrument,
-#            acquired=prod.acquired.datetime,
-#            processed=prod.processed.datetime
-#        )
-#        sql_product, created = get_or_add(session, Product, **product_attrs)
-#        if created:
-#            session.commit()
-#
-#        for band in desired_bands:
-#            # TODO: some sort of product.add_band
-#            band_attrs = dict(
-#                ref_product_id=sql_product.id,
-#                path=band.path,
-#                bidx=band.bidx,
-#                standard_name=band.standard_name,
-#                long_name=band.long_name,
-#                friendly_name=band.friendly_name,
-#                units=band.units,
-#                fill=band.fill,
-#                valid_min=band.valid_min,
-#                valid_max=band.valid_max,
-#                scale_factor=band.scale_factor
-#            )
-#            sql_band, band_created = get_or_add(session, Band, **band_attrs)
-#            if band_created:
-#                session.commit()
-#                # TODO: Actually do the physical work to tile it
-#            else:
-#                print("Already have in database...")
-#
-#    from IPython.core.debugger import Pdb; Pdb().set_trace()
