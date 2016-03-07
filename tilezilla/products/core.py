8a345632e020629ead8a38e2822312bf5cebdf69
Chris Holden <ceholden@gmail.com>
2016-03-07 13:40:55 -0500

    Reorg products
    * Not init from attrs
    * from_path takes place of old init
    * Separate ESPA metadata to sensors/

--- tilezilla/products/core.py
+++ tilezilla/products/core.py
@@ -3,19 +3,63 @@
 Defines the class attributes and methods required for a product.
 """
 import abc
+import textwrap
+
+import six
 
 
-class BaseProduct(object):
+class BaseProduct(six.with_metaclass(abc.ABCMeta)):
     """ Product interface for ``tilezilla``
 
     Args:
         path (str): the path to the root directory of the extracted data
             product
+        bands (list[Band]): List of :class:`Band` for this dataset
+        metadata (dict): Dictionary of metadata about this product
     """
-    __metaclass__ = abc.ABCMeta
 
-    def __init__(self, path):
-        self.path = path
+    def __repr__(self):
+        s = """
+        Product: {desc}
+
+        Scene ID: {scene_id}
+        Acquisition date/time: {acquired}
+        Processing date/time: {processed}
+        Bounding Box:
+            Top:        {uly}
+            Left:       {ulx}
+            Bottom:     {lry}
+            Right:      {lrx}
+
+        Bands: {nbands}
+        {band_names}
+        """.format(
+            desc=self.description,
+            scene_id=self.timeseries_id,
+            acquired=self.acquired.datetime,
+            processed=self.processed.datetime,
+            uly=self.bounds.top,
+            ulx=self.bounds.left,
+            lry=self.bounds.bottom,
+            lrx=self.bounds.right,
+            nbands=len(self.bands),
+            band_names='\n'.join(
+                [''] + [' ' * 12 + b.long_name for b in self.bands])
+        )
+        return textwrap.dedent(s)
+
+    @classmethod
+    @abc.abstractmethod
+    def from_path(cls, path):
+        """ Return an instance of this product stored at a given path
+        """
+        pass
+
+    @abc.abstractproperty
+    def description(self):
+        """ str: Description of product
+        """
+        return
 
     @abc.abstractproperty
     def bands(self):
@@ -27,7 +71,7 @@ class BaseProduct(object):
     def timeseries_id(self):
         """ str: identifier for this acquisition
         """
-        return
+        pass
 
     @abc.abstractproperty
     def acquired(self):
@@ -37,7 +81,7 @@ class BaseProduct(object):
 
     @abc.abstractproperty
     def processed(self):
-        """ Arrow: date and time of processing
+        """ Arrow: Date and time of processing
         """
         return
 
@@ -54,20 +98,19 @@ class BaseProduct(object):
         return
 
     @abc.abstractproperty
-    def metadata_files(self):
-        """ dict: name and paths to any metadata files for this observation
+    def bounds(self):
+        """ BoundingBox: The bounding box of this product in EPSG:4326
         """
         return
 
-    @abc.abstractmethod
-    def bounding_box(self, crs='EPSG:4326'):
-        """ Return the bounding box of this product in some projection
+    @abc.abstractproperty
+    def metadata(self):
+        """ dict: Dictionary of metadata about this product
+        """
+        return
 
-        Args:
-            crs (str or dict): The coordinate reference system, interpretable
-                by rasterio
-
-        Returns:
-            BoundingBox: bounding box of product
+    @abc.abstractproperty
+    def metadata_files(self):
+        """ dict: name and paths to any metadata files for this observation
         """
         return
--- tilezilla/sensors/landsat.py
+++ tilezilla/sensors/landsat.py
@@ -3,10 +3,16 @@
 from collections import OrderedDict
 import re
 
+import arrow
+from bs4 import BeautifulSoup
+import numpy as np
+import pathlib
 import six
 
 from . import SENSOR_FRIENDLY_NAMES
 from .._util import lazy_property
+from ..core import Band, BoundingBox
+
 
 #: dict[sensor=dict[friedly_name=band number]]
 sensor_bands_friendly_name = {
@@ -52,9 +58,11 @@ class MTL(object):
     """ Landsat "MTL" metadata file
 
     Args:
+        path (str): Path to the metadata file
         data (OrderedDict): metadata contained within the MTL file
     """
-    def __init__(self, data):
+    def __init__(self, path, data):
+        self.path = pathlib.Path(str(path)).resolve()
         self.data = data
 
     @lazy_property
@@ -105,7 +113,7 @@ class MTL(object):
             MTL: Instance of MTL
         """
         with open(path, 'r') as fid:
-            return cls(parse_MTL(fid))
+            return cls(path, parse_MTL(fid))
 
     @classmethod
     def from_fid(cls, fid):
@@ -116,4 +124,117 @@ class MTL(object):
         Returns:
             MTL: Instance of MTL
         """
+        from IPython.core.debugger import Pdb; Pdb().set_trace()
         return cls(parse_MTL(fid))
+
+
+class ESPAMetadata(object):
+    """ ESPA Metadata parsing
+
+    Args:
+        path (str): Path to metadata file
+        data (BeautifulSoup): A BeautifulSoup of the XML metadata
+    """
+    def __init__(self, path, data):
+        self.path = pathlib.Path(str(path)).resolve()
+        self.data = data
+
+    @classmethod
+    def from_file(cls, path):
+        return cls(path, BeautifulSoup(open(str(path)), 'lxml'))
+
+    @property
+    def instrument(self):
+        """ str: instrument taking acquisition measurement
+        """
+        return self.data.find('instrument').text
+
+    @property
+    def platform(self):
+        """ str: platform holding instrument for this acquisition
+        """
+        return self.data.find('satellite').text
+
+    @property
+    def acquired(self):
+        """ Arrow: date and time of acquisition
+
+        The time of this acquisition is taken as the scene center time.
+        """
+        ad = self.data.find('acquisition_date').text
+        ct = self.data.find('scene_center_time').text
+        return arrow.get('{}T{}'.format(ad, ct))
+
+    @property
+    def processed(self):
+        """ Arrow: date and time of processing
+        """
+        return arrow.get(self.data.find('production_date').text)
+
+    @property
+    def solar_azimuth(self):
+        """ float: solar azimuth angle during acqusition
+        """
+        return float(self.data.find('solar_angles').attrs['azimuth'])
+
+    @property
+    def solar_zenith(self):
+        """ float: solar zenith angle during acquisition
+        """
+        return float(self.data.find('solar_angles').attrs['zenith'])
+
+    @property
+    def bounds(self):
+        _xml = self.data.find('bounding_coordinates')
+        return BoundingBox(
+            left=float(_xml.find('west').text),
+            top=float(_xml.find('north').text),
+            bottom=float(_xml.find('south').text),
+            right=float(_xml.find('east').text)
+        )
+
+    @lazy_property
+    def bands(self):
+        bands = []
+        for _xml in self.data.find_all('band'):
+            bands.append(self._xml_to_band(_xml))
+        return bands
+
+    def _xml_to_band(self, xml):
+        """ Parse a bit of XML to a Band
+        """
+        def str2dtype(s, dtype):
+            if not s:
+                return None
+            if dtype.kind in ('u', 'i'):
+                return int(s)
+            else:
+                return float(s)
+        # Names
+        standard_name = xml.get('name')
+        short_name = xml.find('short_name').text
+        long_name = xml.find('long_name').text
+        friendly_name = (description_to_friendly_name(short_name[0:2],
+                                                      standard_name)
+                         or standard_name)
+
+        # Units
+        units = xml.find('data_units').text
+        # Filename path
+        path = str(self.path.parent.joinpath(xml.find('file_name').text))
+        # Numeric info
+        data_type = np.dtype(xml.get('data_type').lower())
+
+        fill = str2dtype(xml.get('fill_value'), data_type)
+        valid_range = xml.find('valid_range')
+        _min = str2dtype(valid_range.get('min'), data_type)
+        _max = str2dtype(valid_range.get('max'), data_type)
+        scale_factor = xml.get('scale_factor')
+        if scale_factor:
+            scale_factor = float(scale_factor)
+
+        return Band(path, 1,
+                    standard_name=standard_name, long_name=long_name,
+                    friendly_name=friendly_name,
+                    units=units, fill=fill,
+                    valid_min=_min, valid_max=_max, scale_factor=scale_factor)
