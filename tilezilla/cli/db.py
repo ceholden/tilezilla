4c13d1fc2ba2312e793d678e1c7ce29a5ed678c2
Chris Holden <ceholden@gmail.com>
2016-03-09 01:39:22 -0500

    Prototype db query CLI

--- tilezilla/cli/db.py
+++ tilezilla/cli/db.py
@@ -3,7 +3,7 @@
 import click
 
 from .options import callback_dict
-from ..db import TABLES
+from ..db import TABLES, Database, DatacubeResource, DatasetResource
 
 opt_db_table = click.argument('table', type=click.Choice(TABLES.keys()))
 
@@ -11,15 +11,17 @@ opt_db_table = click.argument('table', type=click.Choice(TABLES.keys()))
 @click.group(help='Database information and queries')
 @click.pass_context
 def db(ctx):
-    pass
+    # Stick in context for sub-commands
+    ctx.obj['db'] = Database.from_config(ctx.obj['config']['database'])
 
 
 @db.command(short_help='Print database information')
 @opt_db_table
 @click.pass_context
 def info(ctx, table):
+    """ Print table information, like the table fields
+    """
     print('Info: "{}"'.format(table))
-
 
 @db.command(short_help='Search database')
 @opt_db_table
@@ -28,4 +30,12 @@ def info(ctx, table):
               help='Fitler TABLE by attr=value')
 @click.pass_context
 def search(ctx, filter_, table):
+    """ Search a table according to some filters
+
+    Example: search the "product" table for a given product ID
+    """
     print('Search: "{}" where:\n{}'.format(table, filter_))
+    query = ctx.obj['db'].session.query(TABLES[table]).filter_by(**filter_)
+    for query_row in query:
+        from IPython.core.debugger import Pdb; Pdb().set_trace()
+        print(query_row)
