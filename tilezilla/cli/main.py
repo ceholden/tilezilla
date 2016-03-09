2e5708f05eb9b781ad94b14f7d00c6ad1a68e9bf
Chris Holden <ceholden@gmail.com>
2016-03-09 01:16:42 -0500

    Parse config in main CLI

--- tilezilla/cli/main.py
+++ tilezilla/cli/main.py
@@ -5,6 +5,7 @@ import click
 import click_plugins
 
 from .. import __version__
+from ..config import parse_config
 
 _context = dict(
     token_normalize_func=lambda x: x.lower(),
@@ -16,16 +17,22 @@ _context = dict(
                             iter_entry_points('tilez.commands'))
 @click.group(help='tilezilla command line interface',
              context_settings=_context)
-@click.option('--config', envvar='TILEZILLA_CONFIG', show_default=True,
+@click.option('--config', 'config_file',
+              envvar='TILEZILLA_CONFIG', show_default=True,
               type=click.Path(exists=True, dir_okay=False, resolve_path=True),
               help='Configuration file (or, as TILEZILLA_CONFIG envvar)')
 @click.option('--verbose', '-v', is_flag=True, help='Be verbose')
 @click.option('--quiet', '-q', is_flag=True, help='Be quiet')
 @click.version_option(__version__)
 @click.pass_context
-def cli(ctx, config, verbose, quiet):
+def cli(ctx, config_file, verbose, quiet):
+    if not config_file:
+        raise click.BadParameter('Must specify a configuration file',
+                                 param_hint='config_file')
     # Pass configuration file
-    ctx.obj = dict(config_file=config)
+    config = parse_config(config_file)
+    ctx.obj = dict(config=config, config_file=config_file)
+
     # Logging config
     logging.basicConfig()
     logger = logging.getLogger('tilezilla')
