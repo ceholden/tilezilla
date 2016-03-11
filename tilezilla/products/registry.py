8aef82a70a9b9e43c7a042f9db0794fbd4156490
Chris Holden <ceholden@gmail.com>
2016-03-10 22:58:24 -0500

    Add netcdf & 'clover' (nc helper) library deps

--- tilezilla/products/registry.py
+++ tilezilla/products/registry.py
@@ -1,12 +1,4 @@
 """ Products available to ingest by this package
-
-.. todo::
-
-    1. We'll need some kind of registry of available products
-    2. Products should raise exception if file path provided doesn't look
-       like an instance of the product
-    3. If product succeeds, put product class on top of test list to avoid
-       repeated fails for subsequent ingests (i.e., assume all )
 """
 from collections import OrderedDict
 import logging
