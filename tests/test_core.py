ba85d798e38d0c4a71a47dabe7c51e0bdf23572f
Chris Holden <ceholden@gmail.com>
2016-05-04 14:15:16 -0400

    Don't compare 'private' members in test

--- tests/test_core.py
+++ tests/test_core.py
@@ -15,12 +15,16 @@ def test_band(rgb_image, band_idx):
 
     with rasterio.open(rgb_image) as src:
         # Assert the properties are the same
-        props = inspect.getmembers(src, lambda x: not callable(x))
+        props = [(p, v) for p, v in
+                 inspect.getmembers(src, lambda x: not callable(x))
+                 if not p.startswith('_')]
         for prop, value in props:
             assert getattr(band.src, prop) == value
 
         filter_ = (lambda x: not callable(x) and
                              not isinstance(x, rasterio._io.RasterReader))
-        props = inspect.getmembers(rasterio.band(src, band_idx), filter_)
+        props = [(p, v) for p, v in
+                 inspect.getmembers(rasterio.band(src, band_idx), filter_)
+                 if not p.startswith('_')]
         for prop, value in props:
             assert getattr(band.band, prop) == value
